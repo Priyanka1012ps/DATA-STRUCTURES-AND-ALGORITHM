bcb9ef88a3adb3e542e08e5d4a1abace3d36ede2
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2021-11-13 08:48:15 +0530

    Update sort_1_0_2.cpp

--- sort_1_0_2.cpp
+++ sort_1_0_2.cpp
@@ -1,1 +1,31 @@
-
+class Solution
+{
+    public:
+    //Function to sort a linked list of 0s, 1s and 2s.
+    Node* segregate(Node *head) {
+        
+    int countarr[]={0,0,0};
+    Node *curr=head;
+    while(curr!=NULL)
+    {
+        countarr[curr->data]++;
+        curr=curr->next;
+    }
+    Node *temp=head;
+    int i=0;
+    while(temp!=NULL)
+    {
+    if(countarr[i]==0)
+    i++;
+    else
+    {
+        temp->data=i;
+        temp=temp->next;
+        countarr[i]--;
+    }
+    
+        
+    }
+    return head;
+    }
+};
