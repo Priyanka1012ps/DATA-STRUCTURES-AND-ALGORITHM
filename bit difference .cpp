3a776582c4227ab18d67a2e6a7481a1536b715d4
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-03-02 21:41:42 +0530

    Create bit difference .cpp

---
+++ bit difference .cpp
@@ -0,0 +1,15 @@
+class Solution{
+    public:
+    // Function to find number of bits needed to be flipped to convert A to B
+    int countBitsFlip(int a, int b){
+        
+        int c=0;
+      
+     while(x)
+     {
+         x = x & (x-1);
+         c++;
+     }
+        return c;
+    }
+};
