b68c889f60a358e648ca387b194fd48b961389e6
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-04-04 00:39:51 +0530

    Time: 12 ms (7.06%), Space: 9.1 MB (30.33%) - LeetHub

--- 88-merge-sorted-array/88-merge-sorted-array.cpp
+++ 88-merge-sorted-array/88-merge-sorted-array.cpp
@@ -5,20 +5,15 @@ public:
         int p=m+n-1;
          for(int i=0;i<nums2.size();i++)
        {
-             ;
-            // if(i>=m)
-            // {
+            
                int k=0;
            nums1[p]=nums2[i];
-           k++;
+           
            p--;
         }
         sort(nums1.begin(),nums1.end());
        }
-        // for(int i=0;i<nums1.size();i++)
-        // {
-        //     if(nums1[i]<nums2[i])
-        // }
+        
         
     
 };
