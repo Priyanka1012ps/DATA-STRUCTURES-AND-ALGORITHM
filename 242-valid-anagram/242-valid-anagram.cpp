db814b1a63e58bfb65f71f1167d24f35a4dbab13
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-04-14 12:58:46 +0530

    Time: 12 ms (59.18%), Space: 7.5 MB (7.88%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -17,29 +17,3 @@ public:
             return false;
     }
 };
-// // class Solution {
-// // public:
-// //     bool isAnagram(string s, string t) {
-        
-// //          if (s.size() != t.size()) return false;
-
-// //          sort(s.begin(), s.end());
-// //          sort(t.begin(), t.end());
-// //          if(s == t)
-// //              return true;
-// //         return false;
-// //     }
-// // };
-//  if (s.size() != t.size()) return false;
-//         unordered_map<char, int> a, b; // we create two unordered maps
-//         for (int i=0; i<s.size(); i++)
-//         {
-//             a[s[i]]++; // we hash the first string into the first map
-//             b[t[i]]++; // we hash the second string into the second map
-//         }
-        
-//         if(a == b) // if both the maps match, we return true else we return false.
-//             return true;
-//         return false;
-//     }
-// };
