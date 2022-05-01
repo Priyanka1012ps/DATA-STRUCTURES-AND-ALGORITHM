f3d6747bb65befea536cfd3d1c56676d07ee3637
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-05-01 11:06:43 +0530

    Time: 4 ms (28.12%), Space: 6.5 MB (26.06%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -45,30 +45,3 @@ public:
            return true;
     }
 };
-// class Solution {
-// public:
-//     bool backspaceCompare(string s, string t) {
-//         string s1="",t1="";
-//         for(int i=0;i<s.length();i++){
-//             if(s[i]=='#'){
-//                 if(!s1.empty()){
-//                     s1.pop_back();
-//                 }
-//                 else continue;
-//             }
-//             else s1.push_back(s[i]);
-//         }
-//         for(int i=0;i<t.length();i++){
-//             if(t[i]=='#'){
-//                 if(!t1.empty()){
-//                     t1.pop_back();
-//                 }
-//                 else continue;
-//             }
-//             else t1.push_back(t[i]);
-//         }
-//         cout<<s1<<" "<<t1<<endl;
-//         if(s1!=t1) return false;
-//         return true;
-//     }
-// };
