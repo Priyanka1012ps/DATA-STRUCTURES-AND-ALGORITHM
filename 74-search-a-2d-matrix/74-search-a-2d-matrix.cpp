0233b16e0081510a4cc1ba42458720e3133c5a5a
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-07-06 19:03:49 +0530

    Time: 16 ms (6.43%), Space: 9.4 MB (78.84%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.cpp
@@ -3,24 +3,47 @@ public:
 
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         
-        int rowNums = matrix.size();
-        if (rowNums == 0)
-            return false;
-        int columnsNum = matrix[0].size();
-        int l = 0;
-        int r = rowNums * columnsNum - 1;
+//         int rowNums = matrix.size();
+//         if (rowNums == 0)
+//             return false;
+//         int columnsNum = matrix[0].size();
+//         int l = 0;
+//         int r = rowNums * columnsNum - 1;
         
-        while (l <= r)
-        {
-            int m = l + (r - l) / 2;
+//         while (l <= r)
+//         {
+//             int m = l + (r - l) / 2;
 
-            int res = matrix[m / columnsNum][m % columnsNum];
+//             int res = matrix[m / columnsNum][m % columnsNum];
             
-            if (res < target)
-                l = m+1;
-            else if (res > target)
-                r = m-1;     
-            else if (res == target)
+//             if (res < target)
+//                 l = m+1;
+//             else if (res > target)
+//                 r = m-1;     
+//             else if (res == target)
+//                 return true;
+//         }
+//         return false;
+        int rows=matrix.size();
+        int cols=matrix[0].size();
+int l=0;
+        int h=(rows*cols)-1;
+        
+        while(l<=h)
+        {
+            int  mid=(l+h)/2;
+            int res= matrix[mid/cols][mid%cols];
+            if(target<res)
+            {
+                h=mid-1;
+                // l++;
+            }
+            else if(target>res)
+            {
+                l=mid+1;
+                
+            }
+            else
                 return true;
         }
         return false;
