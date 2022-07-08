5e1de57c03e10f8c04f85c60b1aaae449f7139a9
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-07-08 23:15:02 +0530

    Time: 93 ms (77.66%), Space: 10.6 MB (98.14%) - LeetHub

---
+++ 1473-paint-house-iii/1473-paint-house-iii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int dp[101][21][101];
+    int minCost(vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target) {
+        memset(dp,-1,sizeof(dp));
+        int ans = helper(0,0,0,houses,cost,m,n,target);
+        if(ans == INT_MAX/2)
+        {
+            return -1;
+        }
+        return ans;
+    }
+    private:
+    int helper(int i,int l,int t,vector<int>& houses, vector<vector<int>>& cost, int m, int n, int target)
+    {
+        if(t>target)
+        {
+            return INT_MAX/2;
+        }
+        if(dp[i][l][t] != -1)
+        {
+            return dp[i][l][t];
+        }
+        if(i == m)
+        {
+            return (t == target)?0:INT_MAX/2;
+        }
+        else
+        {
+            int  ans = INT_MAX/2;
+            if(houses[i] == 0)
+            {
+                for(int j=0;j<n;j++)
+                {
+                    ans = min(ans,cost[i][j]+helper(i+1,j+1,(l == j+1)?t:t+1,houses,cost,m,n,target));
+                }
+            }
+            else
+            {
+                ans = min(ans,helper(i+1,houses[i],(houses[i] == l)?t:t+1,houses,cost,m,n,target));
+            }
+            return dp[i][l][t] = ans;
+        }
+    }
+};
