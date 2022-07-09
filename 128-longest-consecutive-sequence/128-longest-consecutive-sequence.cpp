e1c8132b70ffbf632d3b9cc3da895cc145d6d955
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-07-09 23:24:40 +0530

    Time: 1074 ms (12.13%), Space: 49.7 MB (49.56%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.cpp
@@ -1,32 +1,59 @@
 class Solution {
 public:
     int longestConsecutive(vector<int>& nums) {
-        unordered_set<int> s(nums.begin(),nums.end()); 
+//         unordered_set<int> s(nums.begin(),nums.end()); 
         
        
         
-        int longest_sequence = 0;
+//         int longest_sequence = 0;
         
+//         for(int i=0; i<nums.size(); i++)
+//         {
+//             if(s.find(nums[i] - 1)!=s.end())
+//                 continue;
+            
+//             else
+//             {
+//                 int count = 0;
+//                 int current_element = nums[i];
+                
+//                 while(s.find(current_element) != s.end())
+//                 {
+//                     count++;
+//                     current_element++;
+//                 }
+                
+//                 longest_sequence = max(longest_sequence,count);
+//             }
+//         }
+        
+//         return longest_sequence;
+//     }
+        int ans=0;
+    unordered_set<int> s;
+        for(int i=0; i<nums.size();i++)
+        {
+            s.insert(nums[i]);
+        }
         for(int i=0; i<nums.size(); i++)
         {
-            if(s.find(nums[i] - 1)!=s.end())
+            if(s.find(nums[i]-1)!=s.end())
                 continue;
-            
             else
             {
-                int count = 0;
-                int current_element = nums[i];
-                
-                while(s.find(current_element) != s.end())
+               int current= nums[i];
+                int count=0;
+                while(s.find(current)!=s.end())
                 {
+                    current++;
                     count++;
-                    current_element++;
                 }
-                
-                longest_sequence = max(longest_sequence,count);
+                 ans=max(ans,count);
             }
+         
         }
-        
-        return longest_sequence;
+         
+        return ans;
     }
+        
 };
