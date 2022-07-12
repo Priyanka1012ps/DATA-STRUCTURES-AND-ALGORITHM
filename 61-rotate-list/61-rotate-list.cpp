4268d7d150d9bd21644c3a1eb5e24b56055fa118
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-07-12 23:09:39 +0530

    Time: 24 ms (6.22%), Space: 11.8 MB (5.68%) - LeetHub

--- 61-rotate-list/61-rotate-list.cpp
+++ 61-rotate-list/61-rotate-list.cpp
@@ -12,32 +12,51 @@ class Solution {
 public:
 ListNode* rotateRight(ListNode* head, int k) {
 
-    if(head==NULL || head->next==NULL) return head;
+//     if(head==NULL || head->next==NULL) return head;
     
-    ListNode* ptr=head;
-    int length=0;
+//     ListNode* ptr=head;
+//     int length=0;
     
-    while(ptr!=NULL){
-        length++;
-        ptr=ptr->next;
+//     while(ptr!=NULL){
+//         length++;
+//         ptr=ptr->next;
+//     }
+    
+//     k=k%length;
+    
+//     while(k--){
+        
+//         ptr=head;
+//         ListNode* temp=NULL;
+        
+//         while(ptr->next!=NULL){
+//             temp=ptr;
+//             ptr=ptr->next;
+//         }
+        
+//         ptr->next=head;
+//         temp->next=NULL;
+//         head=ptr;
+//     }
+//     return head;
+    if(head==NULL or head->next==NULL or k==0)
+        return head;
+    ListNode*curr=head;
+    int l=1;
+    while(curr!=NULL and curr->next!=NULL)
+    {
+        curr=curr->next;
+        l++;
     }
-    
-    k=k%length;
-    
-    while(k--){
-        
-        ptr=head;
-        ListNode* temp=NULL;
-        
-        while(ptr->next!=NULL){
-            temp=ptr;
-            ptr=ptr->next;
-        }
-        
-        ptr->next=head;
-        temp->next=NULL;
-        head=ptr;
+    curr->next=head;
+    k=k%l;
+    k = l-k;
+    while(k--)
+    {
+        curr=curr->next;
     }
+    head=curr->next;
+    curr->next=NULL;
     return head;
 }
 };
