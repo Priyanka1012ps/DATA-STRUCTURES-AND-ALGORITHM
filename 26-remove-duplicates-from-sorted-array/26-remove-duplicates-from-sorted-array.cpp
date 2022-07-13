84cd6088703aa927fe2e28b8d39f614571d24877
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-07-13 13:15:39 +0530

    Time: 12 ms (84.27%), Space: 18.3 MB (74.77%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.cpp
@@ -2,25 +2,37 @@ class Solution {
 public:
     int removeDuplicates(vector<int>& nums) 
     {
-        int n=nums.size();
-        int arr[n];
-        int k=0;
-        unordered_map<int,int>mp;
-        for(int i=0;i<nums.size();i++)
-        {
-            mp[nums[i]]++;
-        }
-        nums.clear();
-        for(auto i :mp)
-        {
-            // if(i->second>1)
-            // {
-                nums.push_back(i.first);
-                k++;
+//         int n=nums.size();
+//         int arr[n];
+//         int k=0;
+//         unordered_map<int,int>mp;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             mp[nums[i]]++;
+//         }
+//         nums.clear();
+//         for(auto i :mp)
+//         {
+//             // if(i->second>1)
+//             // {
+//                 nums.push_back(i.first);
+//                 k++;
             
-         }
-        sort(nums.begin(),nums.end());
-        return k;
-       
+//          }
+//         sort(nums.begin(),nums.end());
+//         return k;
+        if(nums.size()==0)
+            return 0;
+        int i=0;
+        for(int j=1;j<nums.size();j++)
+        {
+            if(nums[i]!=nums[j])
+            {
+                i++;
+        
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
     }
 };
