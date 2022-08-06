adc5de6b6fb2030f933d7edfaaab0f25704f1d57
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-08-07 04:16:39 +0530

    Time: 47 ms (49.24%), Space: 24.1 MB (87.58%) - LeetHub

--- 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.cpp
@@ -12,23 +12,33 @@
 class BSTIterator {
 public:
     stack<TreeNode*>st;
-    BSTIterator(TreeNode* root) {
-        func(st,root);
-    }
-    void func(stack<TreeNode*>&st,TreeNode* root){
-        while(root){
-            st.push(root);
-            root=root->left;
-        }
-    }
-    int next() {
-        TreeNode* tp=st.top();
-        st.pop();
-        func(st,tp->right);
-        return tp->val;
+    BSTIterator(TreeNode* root) 
+    {
+        pushAll(root);
     }
     
-    bool hasNext() {
-        return (st.size()!=0);
+    int next() 
+    {
+        TreeNode* temp= st.top();
+        st.pop();
+        pushAll(temp->right);
+        return temp->val;
+        
+    }
+    
+    bool hasNext() 
+    {
+        return !st.empty();
+    }
+    void pushAll(TreeNode* node)
+    {
+        for(;node!=NULL;st.push(node),node=node->left);
     }
 };
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
