2ea8c7a77f27a46fcc4074a57d4a6efea6e371eb
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-08-12 02:44:12 +0530

    Time: 16 ms (58.25%), Space: 8.2 MB (21.24%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    int romanToInt(string str) {
-       
-        map<char, int> roman;
-        roman. insert(make_pair('I', 1));
-        roman.insert(make_pair('V', 5));
-        roman. insert(make_pair('X', 10));
-        roman.insert(make_pair('L', 50));
-        roman.insert(make_pair('C', 100));
-        roman.insert(make_pair('D', 500));
-        roman.insert(make_pair('M', 1000));
-        int len = str.length(),num, sum=0;
-        for(int i=0; i<len; )
+    int romanToInt(string s)
+    {
+        map<char,int> mp={{'I',1},{'V',5},{'X',10} , {'L',50} , {'C',100} ,{'D',500},{'M',1000}} ;
+        int n=s.size();
+        int total=mp[s.back()];
+        for(int i=n-2;i>=0;i--)
         {
-            if(i== (len -1) || (roman[str[i]] >= roman[str[i+1]]))
-            {
-               num =roman[str[i]];
-                 i++;
-            }
-            else{
-               num= roman[str[i+1]] - roman[str[i]];
-                 i=i+ 2;
-            }
-            sum = sum + num;
-            }
-        return sum;
-
-        
+            if(mp[s[i]]<mp[s[i+1]])
+                total-=mp[s[i]];
+            else
+                total+=mp[s[i]];
+        }
+        return total;
     }
+    
 };
