54bc9ccfa92d604472420360c51259f890f43d38
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-09-11 22:16:09 +0530

    Time: 207 ms (18.13%), Space: 34.9 MB (99.12%) - LeetHub

---
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxPerformance(int n, vector<int>& speed, vector<int>& eff, int k) {
+        int mod = 1e9 + 7;
+        vector<int> idx(n);
+        iota(begin(idx), end(idx), 0);  // use this index array to indirectly sort the efficiency and speed arrays
+        sort(begin(idx), end(idx), [&](const auto & a, const auto & b)
+             {
+                 return eff[a] < eff[b];
+             });
+        
+        priority_queue<int, vector<int>, greater<int>> q;
+        long long res = 0, sum = 0;
+        for (int i=n-1; i>=0; --i)
+        {
+            q.push(speed[idx[i]]), sum += speed[idx[i]];  // maintain the max sum in a rolling manner
+            if (q.size() > k)
+            {
+                sum -= q.top(); q.pop();
+            }
+            res = max(res, sum * (long long)eff[idx[i]]);
+        }
+        return res % mod;
+    }
+};
