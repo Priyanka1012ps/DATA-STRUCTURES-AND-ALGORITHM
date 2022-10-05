1efcf2bdc42537434f6d4c2ea31cd0f45d6fc909
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-10-05 22:32:39 +0530

    Update 1-two-sum.cpp

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -12,9 +12,9 @@ public:
         //         ans.push_back(i);
         //         return ans;
         //     }
-        //     mpp[nums[i]]=i;
+        //     mpp[nums[i]]=i :
         // }
-        // return ans;
+        // return ans
         vector<int>ans
         unordered_map<int,int>mp;
         for(int i=0;i<nums.size();i++)
