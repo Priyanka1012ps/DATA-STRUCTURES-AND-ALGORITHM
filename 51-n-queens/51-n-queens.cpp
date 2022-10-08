fccc676ad4d80f30c98b3a07b27d533eeede1c43
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-10-09 00:05:55 +0530

    Time: 4 ms (90.83%), Space: 7.3 MB (66.24%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,50 +1,58 @@
 class Solution {
 public:
-    bool isSafe(int row, int col,vector<string>&board, vector<vector<string>>&ans, int n)
-    {
-        int duprow=row;
-        int dupcol=col;
-        while(col>=0 and row>=0)
-        {
-            if(board[row][col]=='Q')
-                return false;
-               col--;
-               row--;
-        }
-        row=duprow;
-        col=dupcol;
-        while(col>=0)
-        {
-            if(board[row][col]=='Q')
-                return false;
-            col--;
-        }
-        row=duprow;
-        col=dupcol;
-       while(col>=0 and row<n)
-        {
-            if(board[row][col]=='Q')
-                return false;
-            col--;
-           row++;
-        }
-       return true;
+//     bool isSafe(int row, int col,vector<string>&board, vector<vector<string>>&ans, int n)
+//     {
+//         int duprow=row;
+//         int dupcol=col;
+//         while(col>=0 and row>=0)
+//         {
+//             if(board[row][col]=='Q')
+//                 return false;
+//                col--;
+//                row--;
+//         }
+//         row=duprow;
+//         col=dupcol;
+//         while(col>=0)
+//         {
+//             if(board[row][col]=='Q')
+//                 return false;
+//             col--;
+//         }
+//         row=duprow;
+//         col=dupcol;
+//        while(col>=0 and row<n)
+//         {
+//             if(board[row][col]=='Q')
+//                 return false;
+//             col--;
+//            row++;
+//         }
+//        return true;
             
-    }
-    void solve(int col,vector<string>&board,vector<vector<string>>&ans, int n)
+//     }
+    void solve(int col,vector<int>&leftrow,vector<int>&upperdia,vector<int>&lowerdia,vector<string>&board,vector<vector<string>>&ans, int n)
     {
         if(col==n)
         {
             ans.push_back(board);
+             
         return;
         }
         for(int row=0;row<n;row++)
         {
-            if(isSafe(row,col,board,ans,n))
+            if(leftrow[row]==0 and upperdia[row+col]==0 and lowerdia[n-1+ col-row]==0)
             {
                 board[row][col]='Q';
-                solve(col+1,board,ans,n);
+               
+                leftrow[row]=1;
+                upperdia[row+col]=1;
+                lowerdia[n-1 +col-row]=1;
+                 solve(col+1,leftrow,upperdia,lowerdia,board,ans,n);
                 board[row][col]='.';
+                leftrow[row]=0;
+                upperdia[row+col]=0;
+                lowerdia[n-1 +col-row]=0;
             }
         }
     }
@@ -55,7 +63,10 @@ public:
         string s(n,'.');
         for(int i=0;i<n;i++)
             board[i]=s;
-        solve(0,board,ans,n);
+        vector<int>leftrow(n,0),upperdia(2*n-1,0),lowerdia(
+2*n-1,0);
+        
+        solve(0,leftrow,upperdia,lowerdia,board,ans,n);
         return ans;
         
     }
