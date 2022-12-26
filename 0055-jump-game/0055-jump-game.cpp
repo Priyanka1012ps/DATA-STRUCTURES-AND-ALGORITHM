e1d058c292c154588ed5474b85ff2ada6f7617fb
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2022-12-26 21:45:22 +0530

    Time: 63 ms (95.48%), Space: 48.4 MB (48.80%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        
+        int n = nums.size()- 1;
+
+        int lastpos = n;
+
+        for(int i = n-1 ; i >= 0 ; i--)
+        {
+            if(i + nums[i] >= lastpos)
+            {
+                lastpos = i;
+            }
+        }
+
+        return lastpos == 0;
+
+    }
+};
