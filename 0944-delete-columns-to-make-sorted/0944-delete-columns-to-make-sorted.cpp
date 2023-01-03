2b11975bd475fab2e74bd21d0e4dcdc0e0c88098
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2023-01-03 22:16:10 +0530

    Time: 90 ms (51.76%), Space: 12.1 MB (95.24%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        int ans = 0;
+        for(int i = 0; i<m; i++)
+        {
+            char prev = 'a';
+            for(int j = 0; j<n; j++)
+            {
+                if(strs[j][i] >= prev)
+                {
+                    prev = strs[j][i];
+                }
+                else{
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
