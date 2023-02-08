02511aaf16bbb2d47478d26f38ecde801ad7f1a5
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2023-02-08 21:28:22 +0530

    Time: 15 ms (84.34%), Space: 16.7 MB (56.17%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int jump(vector<int>&arr) 
+    {
+        int n=arr.size();
+        int max_R=arr[0];
+        int step=arr[0];
+        int jump=1;
+        if(n==1) return 0;
+        if(n>=1 && arr[0]==0)return -1;
+        for(int i=1;i<n;i++)
+        {
+            
+            if(i==n-1)return jump;
+           
+            max_R=max(max_R,i+arr[i]);
+            step--;
+            if(step==0)
+            {
+                jump++;
+                if(i>=max_R)return -1;
+                step=max_R-i;
+            }
+        }
+        return jump;
+        
+    }
+};
