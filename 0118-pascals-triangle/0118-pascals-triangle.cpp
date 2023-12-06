96e18578ca31439bc5b07823503a005bc8bce084
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2023-12-06 17:45:46 +0530

    Time: 4 ms (25.44%), Space: 7.2 MB (25.08%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,43 +1,30 @@
 class Solution {
 public:
-    vector<vector<int>> generate(int num)
+    
+    int ncr(int n, int r)
     {
-        vector<vector<int>>ans;
-   
-//         for(int i=0;i<num;i++)
-//         {
-//                  vector<int>row(i+1,1);
-//             for(int j=1;j<i;j++)
-//             {
-//                 row[j] = ans[i-1][j] + ans[i-1][j-1];
-//             }
-//             ans.push_back(row);
-       
-//     }
-//         return ans;
-//     }
-    //     for(int i=0;i<num;i++)
-    //     {
-    //         vector<int>v(i+1,1);
-    //         for(int j=1;j<i;j++)
-    //         {
-    //             v[j]=ans[i-1][j]+ans[i-1][j-1];
-    //         }
-    //         ans.push_back(v);
-    //     }
-    //     return ans;
-    // }
-    vector<vector<int>>r(num);
-    for(int i=0;i<num;i++)
-    {
-        r[i].resize(i+1);
-        r[i][0]=r[i][i]=1;
-        for(int j=1;j<i;j++)
+      long long ans=1;
+        for(int i=0;i<r;i++)
         {
-            r[i][j]=r[i-1][j]+r[i-1][j-1];
+        ans=ans*(n-i);
+        ans=ans/(i+1);
         }
+        return (int)ans;
     }
-    return r;
-
+    vector<vector<int>> generate(int row) {
+        
+         vector<vector<int>>ans;
+       
+        
+        for(int i=1;i<=row;i++)
+        {   vector<int>temp;
+            for(int j=1;j<=i;j++)
+            {
+                temp.push_back(ncr(i-1,j-1));
+            }
+              ans.push_back(temp);
+        }
+      
+        return ans;
     }
 };
