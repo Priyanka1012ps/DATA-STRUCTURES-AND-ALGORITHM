d8e93c34878360f238bc2c519933a41047c6b3be
PRIYANKA SINGH <72395482+Priyanka1012ps@users.noreply.github.com>
2024-07-30 19:40:33 +0530

    Time: 76 ms (89.95%), Space: 23.7 MB (73.37%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minimumDeletions(string s) {
+        
+        int aCount = 0;
+        for(auto &ch : s) aCount += (ch == 'a');
+        int minDeletions = INT_MAX;
+        int bCount = 0;
+        for(auto &ch : s){
+            aCount -= (ch == 'a');
+            minDeletions = min(minDeletions, aCount + bCount);
+            bCount += (ch == 'b');
+        }
+        return minDeletions;
+    }
+};
